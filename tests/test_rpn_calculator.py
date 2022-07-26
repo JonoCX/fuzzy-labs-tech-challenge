bfa0eb8fc97aa3cd4780c4387ec4c6492ba87183
Jonathan Carlton <jonathan.carlton@hotmail.com>
2022-07-26 20:53:53 +0100

    added tests for input

--- tests/test_rpn_calculator.py
+++ tests/test_rpn_calculator.py
@@ -133,3 +133,142 @@ class TestCalculations:
 
         # the result should be 0
         assert int(out) == 0
+
+
+class TestInput:
+
+    @pytest.fixture(autouse = True)
+    def capsys(self, capsys):
+        """ This fixture enables us to capture the output to the terminal """
+        self.capsys = capsys
+
+    
+    def test_input_works_as_expected(self):
+        """ Test that the input works as expected before testing for other things """
+        # this will trigger the calculator to perform the calculation
+        rpn.main('5 2 /')
+
+        # capture the output of the program
+        out, _ = self.capsys.readouterr()
+
+        # the result should be 2
+        assert int(out) == 2
+
+    def test_empty_input(self):
+        """ 
+            Test that the calculator prints an appropriate message and 
+            exits when an empty input is passed.
+        """
+        with pytest.raises(SystemExit) as empty_exit:
+            # this will trigger the message to be printed and a sys.exit to occur
+            rpn.main('') 
+
+            # capture that commandline output and assert that it's what we expect
+            out, _ = self.capsys.readouterr()
+
+            assert out == 'No RPN expression provided.'
+        
+        # check that the type of exception through is correct
+        assert empty_exit.type == SystemExit
+
+
+    def test_that_system_exits_when_single_input_given(self):
+        """
+            Test that the appropriate error message is present to the user and
+            that the system exits when only a single input is given, i.e., '5'
+        """
+        with pytest.raises(SystemExit) as single_input_error:
+            # this will trigger the error message to appear and the system to exit
+            rpn.main('5')
+
+            # capture the output to the command line
+            out, _ = self.capsys.readouterr()
+
+            # assert that the it is the correct error message
+            assert out == "Only a single value has been inputted: '5'; " \
+                            "this could be due to the lack of of spaces in the expression"
+
+        # check that the type of error is correct
+        assert single_input_error.type == SystemExit
+
+
+    
+    def test_that_input_with_no_spaces_is_rejected(self):
+        """
+            Test that when the user provides an expression where there are no spaces,
+            the calculator detects it, prints the error message, and exists
+        """
+        with pytest.raises(SystemExit) as no_spaces_error:
+            # this will trigger the error message to appear and the system to exit
+            rpn.main('52/')
+
+            # capture the output to the command line
+            out, _ = self.capsys.readouterr()
+
+            # assert that it is the correct error message
+            assert out == "Only a single value has been inputted: '52/'; " \
+                        "this could be due to the lack of of spaces in the expression.\n"
+
+        # check that the type of error is correct
+        assert no_spaces_error.type == SystemExit
+
+
+    def test_that_only_allowed_operations_can_be_provided(self):
+        """
+            Test that the calculator only accepts the operators (+ - * / %). For example, 
+            it isn't suppose to accept expressions with exponentiation.
+        """
+        with pytest.raises(SystemExit) as invalid_operator_error:
+            # this will trigger the error message to appear as it's attempting to calculate
+            # an exponentiation.
+            rpn.main('5 2 **')
+
+            # capture the output to the command line
+            out, _ = self.capsys.readouterr()
+
+            # assert that it is the correct error message
+            assert out == "Invalid input given: '**'; only whole numbers are accepted " \
+                            "and (+ - * / %) are the only operators allowed.\n"
+
+        # check that the type of error is correct
+        assert invalid_operator_error.type == SystemExit
+
+
+    def test_that_text_instead_of_numbers_is_rejected(self):
+        """
+            Test that when the user provides an expression which includes something
+            that is not a number, i.e., 'two' instead of 2, that an appropriate
+            message is displayed and that the system exits
+        """
+        with pytest.raises(SystemExit) as inappropriate_input_exit:
+            # this will trigger an output which tells the user they cannot have 'two' in the input
+            rpn.main('5 two /')
+
+            # capture the commandline output
+            out, _ = self.capsys.readouterr()
+
+            # assert that the output matches what we would expect
+            assert out == "Invalid input given: 'two'; only whole numbers are accepted " \
+                            "and (+ - * / %) are the only operators allowed.\n"
+        
+        # check that the system exits
+        assert inappropriate_input_exit.type == SystemExit
+
+
+    def test_that_only_whole_numbers_are_accepted(self):
+        """
+            Test that only whole numbers are accepted, i.e., 2.5 should not be accepted as
+            part of an expression.
+        """
+        with pytest.raises(SystemExit) as inappropriate_input_exit:
+            # this will trigger an output which tells the users they cannot have 2.5 in the input
+            rpn.main('5 2.5 /')
+            
+            # capture the commandline output
+            out, _ = self.capsys.readouterr()
+
+            # assert that the output matches what we would expect
+            assert out == "Invalid input given: '2.5'; only whole numbers are accepted.\n"
+
+        # assert that system exits under type of input
+        assert inappropriate_input_exit.type == SystemExit
