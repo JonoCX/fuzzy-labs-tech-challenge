85393a838753f82d1fa9422950b7e9b4320ebc68
Jonathan Carlton <jonathan.carlton@hotmail.com>
2022-07-26 20:10:13 +0100

    implemented calculator with tests

--- calculator/rpn_calculator.py
+++ calculator/rpn_calculator.py
@@ -4,6 +4,7 @@
     Taking an input from the user via the command line, the program
     calculates the result. For example, "5 2 /" => 2
 """
+import sys
 
 def _add(num_one: int, num_two: int) -> int:
     """
@@ -55,3 +56,86 @@ def _modulo(num_one: int, num_two: int) -> int:
     """
     return num_one % num_two
 
+"""
+    Create a mapping between the operations that are allowed to be 
+    inputted by the user and their corresponding functions. 
+    Also acts as a way to check for valid input.
+"""
+operations = {
+    '+': _add, 
+    '-': _subtract, 
+    '*': _multiply, 
+    '/': _division, 
+    '%': _modulo
+}
+
+
+def _calculator(in_expression):
+    """
+        The RPN calculator. 
+        Given an input, e.g. "5 2 /", the calcuator returns the result, e.g. 2
+        :params in_expression: the input into the calculator - the expression that 
+                                is to be calculated
+        :return: the result of carrying out the expression
+    """
+    # split the input into its components, e.g., "5 2 /" -> [5, 2, /]
+    expression_components = in_expression.split()
+
+    if len(expression_components) == 1:
+        print(f"Only a single value has been inputted: '{expression_components[0]}'; this could" \
+            " be due to a lack of spaces in the expression.")
+        sys.exit()
+
+    stack = []
+    for part in expression_components:
+        # if the part is an operation (e.g. /)
+        if part in operations.keys():
+            
+            # get the second number (e.g., 2)
+            num_two = stack.pop()
+
+            # get the first number (e.g., 5)
+            num_one = stack.pop()
+
+            # use the operator to get the correct function and pass the two numbers
+            result = operations[part](num_one, num_two)
+
+            # add the result to the stack
+            stack.append(result)
+        # otherwise it's a number
+        else: 
+            # try to convert the input into an integer
+            try:
+                # and add to the stack
+                stack.append(int(part))
+            # if it is anything other than a whole number (float or text), 
+            # an exception is thrown and caught
+            except ValueError:
+                # Report back the suspected error to the user and terminate the program
+                print(f"Invalid input given: '{part}'; only whole numbers are accepted " \
+                    "and (+ - * / %) are the only operators allowed.")
+                sys.exit()
+    
+    # return the result
+    return stack.pop()
+
+            
+def main(in_expression):
+    """
+        The main function where the calculator is called based on the input from the
+        user
+        :params in_expression: the input from the user
+    """
+    # the user has supplied an empty input
+    if not in_expression:
+        print('No RPN expression provided.')
+        sys.exit()
+
+    # fetch the result and print it
+    result = _calculator(in_expression)
+    print(result)
+
+
+if __name__ == '__main__':
+    in_expression = input()
+    main(in_expression)
--- tests/test_rpn_calculator.py
+++ tests/test_rpn_calculator.py
@@ -8,6 +8,12 @@ import calculator.rpn_calculator as rpn
 
 class TestCalculations:
 
+    @pytest.fixture(autouse = True)
+    def capsys(self, capsys):
+        """ This fixture enables us to capture the output to the terminal """
+        self.capsys = capsys
+
+
     def test_addition_func(self):
         """ Test that the addition function is working as expected """
         assert rpn._add(5, 2) == 7
@@ -31,3 +37,99 @@ class TestCalculations:
     def test_modulo_func(self):
         """ Test that the modulo function is working as expected """
         assert rpn._modulo(5, 2) == 1
+
+
+    def test_addition(self):
+        """ Test the addition function with an RPN expression """
+        # call the main function to mock input into the calculator
+        rpn.main('10 5 +')
+
+        # capture the output to the terminal
+        out, _ = self.capsys.readouterr()
+
+        # the result should be 15
+        assert int(out) == 15 
+
+
+    def test_substraction(self):
+        """ Test the subtraction function with an RPN expression """
+        # call the main function to mock input 
+        rpn.main('10 5 -')
+
+        # capture the output to the terminal
+        out, _ = self.capsys.readouterr()
+
+        # the result should be 5
+        assert int(out) == 5
+
+
+    def test_multiplication(self):
+        """ Test the multiplication function an RPN expression """
+        # call the main function to mock input
+        rpn.main('10 5 *')
+
+        # capture the output to the terminal
+        out, _ = self.capsys.readouterr()
+
+        # the result should be 50
+        assert int(out) == 50
+
+
+    def test_division(self):
+        """ Test the divison function an RPN expression """
+        # call the main function to mock input
+        rpn.main('5 2 /')
+
+        # capture the output to the terminal
+        out, _ = self.capsys.readouterr()
+
+        # the result should be 2
+        assert int(out) == 2
+
+
+    def test_modulo(self):
+        """ Test the modulo function an RPN expression """
+        # call the main function to mock input
+        rpn.main("5 2 %")
+
+        # capture the output to the terminal
+        out, _ = self.capsys.readouterr()
+
+        # the result should be 1
+        assert int(out) == 1
+
+
+    def test_multiple_operation_expression(self):
+        """ Test a slightly more complicated expression with multiple operations """
+        # call the main function to mock input
+        rpn.main('4 7 + 2 *')
+
+        # capture the output to the terminal
+        out, _ = self.capsys.readouterr()
+
+        # the result should be 22
+        assert int(out) == 22
+
+
+    def test_multiple_operation_expression_complex(self):
+        """ Test a complex expression with multiple operations """
+        # call the main function to mock input
+        rpn.main('2 3 * 11 14 * +')
+
+        # capture the output to the terminal
+        out, _ = self.capsys.readouterr()
+
+        # the result should be 160
+        assert int(out) == 160        
+
+
+    def test_multiple_operation_expression_complex_equal_zero(self):
+        """ Test a complex expression where the answer should be 0 (but is 0.33 with infix) """
+        # call the main function to mock input
+        rpn.main('5 4 * 2 3 * / 7 2 % 4 - +')
+
+        # capture the output to the terminal
+        out, _ = self.capsys.readouterr()
+
+        # the result should be 0
+        assert int(out) == 0
